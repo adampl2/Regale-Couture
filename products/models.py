e3595d660208edd021e258ff5ef251a73a9bf361
adampl2 <adamchwiulkowski@gmail.com>
2023-05-25 00:11:47 +0000

    Customized admin

--- products/models.py
+++ products/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 
 class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
     name = models.CharField(max_length=234)
     friendly_name = models.CharField(max_length=234, null=True, blank=True)
 
