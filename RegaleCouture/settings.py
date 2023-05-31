fca7c8ddfb60c9ac670489fa51c02fe5e602037e
adampl2 <adamchwiulkowski@gmail.com>
2023-05-31 12:20:06 +0000

    added category filtering

--- RegaleCouture/settings.py
+++ RegaleCouture/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY =
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost', '8000-adampl2-regalecouture-9cavcjb3oh8.ws-eu98.gitpod.io']
 
 
 # Application definition
