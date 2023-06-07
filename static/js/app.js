dbad579006eec4137fb79c7ce3d2b4259e0a0bb0
adampl2 <adamchwiulkowski@gmail.com>
2023-06-07 15:36:41 +0000

    Added code to handle increment/decrement quantity btns

--- static/js/app.js
+++ static/js/app.js
@@ -19,12 +19,18 @@ $('#sort-selector').change(function () {
     }
 });
 
+
+/**
+ * This function deals with Back to Top button. Button is hidden by default.
+ * Once scroll exceeds 100px, button will appear in a bottom-right corner of the page.
+ */
+
 jQuery(document).ready(function ($) {
     let $toTopBtn = $('#toTopBtn');
 
     // Show/hide the back to top button based on scroll position
     $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) { // Change 100 to the desired scroll position
+        if ($(this).scrollTop() > 100) { // Show button when scroll position at 100px
             $toTopBtn.removeClass('hidden');
         } else {
             $toTopBtn.addClass('hidden');
@@ -38,4 +44,46 @@ jQuery(document).ready(function ($) {
             scrollTop: 0
         }, 'slow');
     });
+});
+
+// Disable +/- buttons outside 1-99 range
+function handleEnableDisable(itemId) {
+    var currentValue = parseInt($(`#id_qty_${itemId}`).val());
+    var minusDisabled = currentValue < 2;
+    var plusDisabled = currentValue > 98;
+    $(`#decrement-qty_${itemId}`).prop('disabled', minusDisabled);
+    $(`#increment-qty_${itemId}`).prop('disabled', plusDisabled);
+}
+
+// Ensure proper enabling/disabling of all inputs on page load
+var allQtyInputs = $('.qty_input');
+for (var i = 0; i < allQtyInputs.length; i++) {
+    var itemId = $(allQtyInputs[i]).data('item_id');
+    handleEnableDisable(itemId);
+}
+
+// Check enable/disable every time the input is changed
+$('.qty_input').change(function () {
+    var itemId = $(this).data('item_id');
+    handleEnableDisable(itemId);
+});
+
+// Increment quantity
+$('.increment-qty').click(function (e) {
+    e.preventDefault();
+    var closestInput = $(this).closest('.input-group').find('.qty_input')[0];
+    var currentValue = parseInt($(closestInput).val());
+    $(closestInput).val(currentValue + 1);
+    var itemId = $(this).data('item_id');
+    handleEnableDisable(itemId);
+});
+
+// Decrement quantity
+$('.decrement-qty').click(function (e) {
+    e.preventDefault();
+    var closestInput = $(this).closest('.input-group').find('.qty_input')[0];
+    var currentValue = parseInt($(closestInput).val());
+    $(closestInput).val(currentValue - 1);
+    var itemId = $(this).data('item_id');
+    handleEnableDisable(itemId);
 });
