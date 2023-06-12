c07806c10865e7692465d5df223e2c450f3b1a63
adampl2 <adamchwiulkowski@gmail.com>
2023-06-12 21:09:12 +0000

    Toast templates moved to correct folder

--- RegaleCouture/settings.py
+++ RegaleCouture/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY =
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['8000-adampl2-regalecouture-9cavcjb3oh8.ws-eu99.gitpod.io']
 
 
 # Application definition
