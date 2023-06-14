5face11009b0933979762e27300d6105c6cf7192
adampl2 <adamchwiulkowski@gmail.com>
2023-06-14 23:22:21 +0000

    Fixed error reg. naming conventions

--- checkout/models.py
+++ checkout/models.py
@@ -35,7 +35,7 @@ class Order(models.Model):
         accounting for delivery costs.
         """
         self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
-        if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
+        if self.order_total < settings.FREE_SHIPPING_THRESHOLD:
             self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
         else:
             self.delivery_cost = 0
