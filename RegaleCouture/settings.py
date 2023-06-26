2fba7472f530101d6b15d35b383575a2b61c9d35
adampl2 <adamchwiulkowski@gmail.com>
2023-06-26 00:54:08 +0000

    setting up emails

--- RegaleCouture/settings.py
+++ RegaleCouture/settings.py
@@ -103,8 +103,6 @@ AUTHENTICATION_BACKENDS = [
 
 SITE_ID = 1
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
@@ -176,7 +174,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
-     # Cache control
+    # Cache control
     AWS_S3_OBJECT_PARAMETERS = {
         'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
         'CacheControl': 'max-age=94608000',
@@ -209,8 +207,19 @@ FREE_SHIPPING_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
 
 STRIPE_CURRENCY = 'gbp'
-STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', 'pk_test_51NIsHyI8ffhfiJ98HWT12zawkjbOn94ebo5y2Wtz4kAXEi8l8OKgCnv1xg1CIeXhhDSn2KgGUz6fdDmBI7xEUewN004Da0hjWt')
-STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', 'sk_test_51NIsHyI8ffhfiJ98XrJTV6DjokkyQ6iXTIOmiDql2olae7jYIjE9f4LNaV7vYbMHFn7ndPCADSp6jO3cYkABqJS500H685F7T0')
-STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', 'whsec_qtBy1MS1jHOwGyMNuo2AII7eAQ1KxnCi')
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
-DEFAULT_FROM_EMAIL = 'rcouture@gmail.com'
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'regalecouture@example.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+
