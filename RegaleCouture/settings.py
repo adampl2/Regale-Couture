05d9c4438bce2635de423dfa7a7f16c88b9d4984
adampl2 <adamchwiulkowski@gmail.com>
2023-07-02 21:00:11 +0000

    Data migration

--- RegaleCouture/settings.py
+++ RegaleCouture/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = "$9*d8ubxh*21eed=&ihu0$elura0oevpmvv#wxzu4kh3)kivvm"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
